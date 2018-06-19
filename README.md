3f89321cee17881a6f04395947bacb4246320ea4
Carlton Li <541559617@qq.com>
2018-06-20 07:44:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# PayPalTest2" 
+# PPassignment
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const ejs = require('ejs');
+//import paypal node sdk
 const paypal = require('paypal-rest-sdk');
 
+//configure a sandbos environment
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
   'client_id': 'AZX_IGFYY6JFsWRD6dib-Y60KINzKNZkpca7vRDWK1ZcqcH15S4FvKJQ7RVU9X0QwwduWq8aIeXOJBHC',
@@ -10,6 +12,7 @@ paypal.configure({
 
 
 const app = express();
+// use bodyparser to parse the information filled in by user/buy in the html form in "index.ejs"
 const bodyParser = require('body-parser');
 
 // create application/json parser
@@ -18,18 +21,24 @@ var jsonParser = bodyParser.json()
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){res.render('index')});
 
+// accept payment information
 app.post('/pay', urlencodedParser, function(req,res){
+
+  // get buyer information from html form in "index.ejs"
 	console.log(req.body.firstname);
-    var recipient_name = req.body.firstname + " " + req.body.lastname;
+  var recipient_name = req.body.firstname + " " + req.body.lastname;
+  console.log(recipient_name);
 	var street = req.body.street;
 	var country_code = req.body.country;
 	var postal_code = req.body.zip;
 	var state = req.body.state;
-
+  
+  // create payment after "buy" button is clicked
 	const create_payment_json = {
     "intent": "sale",
     "payer": {
@@ -44,7 +53,7 @@ app.post('/pay', urlencodedParser, function(req,res){
             "items": [{
                 "name": "Red Sox Hat",
                 "sku": "001",
-                "price": "25",
+                "price": "5",
                 "currency": "USD",
                 "quantity": 1
             }],
@@ -61,12 +70,13 @@ app.post('/pay', urlencodedParser, function(req,res){
         },
         "amount": {
             "currency": "USD",
-            "total": "25.00"
+            "total": "5.00"
         },
         "description": "Hat for the best team ever"
     }]
    };
 
+  // create payment after "buy" button is clicked, use the json variable declared above
 	paypal.payment.create(create_payment_json, function (error, payment) {
 			  if (error) {
 			      throw error;
@@ -80,6 +90,7 @@ app.post('/pay', urlencodedParser, function(req,res){
     });
 });
 
+// execute the payment and redirect to "Thank you" page after user authorized the payment. 
 app.get('/success', (req, res) => {
   const payerId = req.query.PayerID;
   const paymentId = req.query.paymentId;
@@ -89,24 +100,25 @@ app.get('/success', (req, res) => {
     "transactions": [{
         "amount": {
             "currency": "USD",
-            "total": "25.00"
+            "total": "5.00"
         }
     }]
   };
 
+  //excute the payment using the payment json declared above
   paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
     if (error) {
         console.log(error.response);
         throw error;
     } else {
         console.log(JSON.stringify(payment));
-        res.send('Success');
-        res.send(req.query.PayerID);
-        res.send(req.query.paymentId);
+        res.send('Thank you!'+'</br>'+'Your transaction ID is:'+payment.transactions[0].related_resources[0].sale.id);
+        
     }
 });
 });
 
+// display "Cancelled" if the user cancel
 app.get('/cancel', (req, res) => res.send('Cancelled'));
 
 
